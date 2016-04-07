5e9b04415f807522f8765a5f0ce060617b03ee79
Elder Xavier <eldersxavier@gmail.com>
2016-04-07 00:47:56 -0300

    arquivos do aplicativo

---
+++ app/SplashDevMedia/app/src/main/java/br/edu/devmedia/splashscreen/ActivityPrincipal.java
@@ -0,0 +1,14 @@
+package br.edu.devmedia.splashscreen;
+
+/**
+ * Created by cef 9nformatica on 26/03/2016.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+public class ActivityPrincipal extends Activity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
---
+++ banco_de_dados/banco_de_dados.sql
@@ -0,0 +1,6 @@
+USE `u551914078_cadin`; 
+
+CREATE TABLE IF NOT EXISTS `entrada` (
+  `submit_time` decimal(16,4) NOT NULL,
+  PRIMARY KEY (`submit_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
